0166991f49f3b842d0b7584eef7b3f6725d33ef7
Steve Shea <stevenlsjr@gmail.com>
2018-10-15 17:39:01 -0400

    make Model serializers hyperlinked

--- chatroom/serializers.py
+++ chatroom/serializers.py
@@ -2,24 +2,22 @@ from rest_framework import serializers
 from .models import Message, Conversation
 from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'first_name', 'last_name', 'email',
+                  'is_staff', 'date_joined')
+        # exclude = ('password',)
 
-class MessageSerializer(serializers.ModelSerializer):
+class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
 
 
-class ConversationSerializer(serializers.ModelSerializer):
-    
-
+class ConversationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Conversation
-        fields = ('pk', 'host', 'title', 'desc', 'message_set')
+        fields = ('url', 'host', 'title', 'desc', 'message_set')
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        # fields = ('username', 'first_name', 'last_name', 'email', 'is_active',
-        #           'is_staff', 'date_joined', 'last_login')
-        exclude = ('password',)
